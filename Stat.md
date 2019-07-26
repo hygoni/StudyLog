012358f276c4830e24c7862ac029f807d31d2fb6
hygoni <9hrack@naver.com>
2019-07-26 14:44:09 +0000

    added stat system

---
+++ Stat.md
@@ -0,0 +1,6 @@
+Since 2019-07-26### [Algorithm] : 3.5 hours
+
+### [Programming] : 2.0 hours
+
+### [DeepLearning] : 0.1 hours
+
---
+++ stat.py
@@ -0,0 +1,45 @@
+import os
+import re
+
+
+dirs = list()
+
+statName = re.compile('\[.*\]')
+timeStudied = re.compile('([0-9]?\.?[0-9]?\s?hours)')
+
+statDict = dict()
+
+for elem in os.listdir():
+    if elem[:1] == '.': #if it is secret folder
+        continue
+    if os.path.isdir(elem):
+        dirs.append(elem)
+
+for dir in dirs:
+
+    files = os.listdir(dir)
+    for file in files:
+        print(dir + '/' + file)
+        f = open(dir + '/' + file, 'r')
+        text = f.readlines()
+        f.close()
+        for line in text:
+            stat = statName.findall(line)
+            hours = timeStudied.findall(line)
+            
+
+            if len(stat) != 0:
+                if stat[0] not in statDict:
+                    statDict[stat[0]] = 0.0
+                    hours[0] = hours[0].replace(' ', '')
+                    hours[0] = hours[0][:-5]
+                    statDict[stat[0]] += float(hours[0])
+
+output = 'Since 2019-07-26'
+
+for stat in statDict:
+    output += '### {} : {} hours\n\n'.format(stat, statDict[stat])
+    
+f = open('Stat.md', 'w+')
+f.write(output)
+f.close()
